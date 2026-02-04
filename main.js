c3b2ce3e243b7fc1572808f2108f652d05433e6c
boso win <robertoingrao4@gmail.com>
2026-02-04 13:25:03 +0100

    selfwork dom 2

---
+++ main.js
@@ -0,0 +1,17 @@
+// Al click sul bottone, crea un articolo popolato dai valori prelevati dai due input
+
+// inserire nell’articolo anche la data di pubblicazione tramite questa istruzione → Date - JavaScript | MDN
+
+// let date = new Date();
+// let formatDate = date.toLocaleDateString()
+
+// EXTRA:
+// fai in modo che, cliccando sul bottone crea articolo, se titolo o paragrafo sono vuoti, esca un alert che informi l’utente del problema
+// fai in modo che, una volta creato l’articolo, gli input vengano puliti
+
+
+
+// Brainstorming
+// dovro sicuramente usare il metodo create new
+// i valori si potrebbero estrapolare da un oggetto, l'oggetto articolo
+// se faccio un articolo i campi di input si devono azzerare
